3e6b130a78d2d79eec807546a2cb691b948d94c9
Owen-Rose <owen.rose@edu.sait.ca>
2023-12-17 16:59:52 -0700

    commit

--- config/supabaseClient.js
+++ config/supabaseClient.js
@@ -1,5 +1,4 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://hpqdlwztboysjwxaaxqq.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhwcWRsd3p0Ym95c2p3eGFheHFxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDIyNTQxNjYsImV4cCI6MjAxNzgzMDE2Nn0.MibJQkGLUK1X9SsDCuzH3O1SRFg8Og7G062AWyZjEe4";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 export const supabase = createClient(supabaseUrl, supabaseKey);
